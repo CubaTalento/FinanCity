4a09dc715b23453bbdb512939ac9d8507c42b8f1
CubaTalento <danaeaguila0@gmail.com>
2026-07-29 12:37:09 -0400

    Update economia.js

--- frontend/js/economia.js
+++ frontend/js/economia.js
@@ -1,32 +1,72 @@
-// FinanCity - Sistema económico y jugador
+// FinanCity - Sistema económico y guardado
 
-const jugador = {
+let jugador = {
+
     nombre: "Emprendedor",
     nivel: 1,
     experiencia: 0,
     dinero: 1000
+
 };
+
+
+function guardarPartida() {
+
+    localStorage.setItem(
+        "financity_guardado",
+        JSON.stringify(jugador)
+    );
+
+}
+
+
+function cargarPartida() {
+
+    const guardado = localStorage.getItem("financity_guardado");
+
+    if (guardado) {
+        jugador = JSON.parse(guardado);
+    }
+
+}
 
 
 const economia = {
 
     agregarIngreso(cantidad) {
+
         jugador.dinero += cantidad;
+
+        guardarPartida();
+
     },
+
 
     agregarGasto(cantidad) {
+
         jugador.dinero -= cantidad;
+
+        guardarPartida();
+
     },
+
 
     obtenerBalance() {
+
         return jugador.dinero;
+
     },
 
+
     obtenerJugador() {
+
         return jugador;
+
     }
 
 };
 
 
-console.log("Sistema de jugador y economía cargado");
+cargarPartida();
+
+console.log("Sistema económico cargado");
